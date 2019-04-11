35ab99032299ab51ce5a5fbc4dd3187fac4ef2a6
Naoki Kodama <p-naoki.kodama@atmark-techno.com>
2019-04-11 16:48:29 +0900

    Add Grove Air Quality Sensor module

---
+++ grove/Air_Quality_Sensor_v1.3/main.py
@@ -0,0 +1,37 @@
+from machine import ADC
+from time import sleep
+import zcoap
+import ujson
+
+class AirQuality:
+    def __init__(self, port):
+        if port == 0:
+            self.adc = ADC(0)
+        elif port == 1:
+            self.adc = ADC(4)
+        elif port == 2:
+            self.adc = ADC(6)
+        else:
+            raise Exception("Port# out of range")
+
+    def read(self): #TODO: Not making sense as gas concentration
+        return self.adc.read()
+                
+def main():
+    path = 'thing/' + zcoap.eui64()
+    reported = {'state':{'reported':{}}}
+
+    addr = zcoap.gw_addr()
+    port = 5683
+    cli = zcoap.client((addr, port))
+
+    air = AirQuality(0)
+
+    while True:
+        reported['state']['reported']['air_quality'] = air.read()
+
+        cli.request_post(path, ujson.dumps(reported))
+        sleep(60)
+
+if __name__ == "__main__":
+    main()
