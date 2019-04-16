f645812634f7a2c7b30290831f35be5e644d9597
Takumi Ando <takumi.ando@atmark-techno.com>
2019-04-16 14:10:54 +0900

    grove: Add a sample for relay

---
+++ grove/Relay/main.py
@@ -0,0 +1,34 @@
+from machine import Pin
+from machine import Signal
+from machine import ADC
+import zcoap
+import time
+import ujson
+
+if __name__ == '__main__':
+    path = 'thing/' + zcoap.eui64()
+    reported = {'state':{'reported':{}}}
+
+    addr = zcoap.gw_addr()
+    port = 5683
+    cli = zcoap.client((addr, port))
+
+    sw4 = Pin(('GPIO_1', 14), Pin.IN, Pin.PULL_UP)
+    relay = Pin(('GPIO_1', 8), Pin.OUT)
+
+    before = 0
+    while True:
+        value = sw4.value()
+        if value != before:
+            if value == 1:
+                reported['state']['reported']['relay'] = 'off'
+                relay.off()
+            else:
+                reported['state']['reported']['relay'] = 'on'
+                relay.on()
+
+            print(ujson.dumps(reported))
+            cli.request_post(path, ujson.dumps(reported))
+            before = value
+
+    cli.close()
