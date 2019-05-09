7b813a6b4ffee58887c1bae11488919da779884c
Naoki Kodama <p-naoki.kodama@atmark-techno.com>
2019-05-09 17:09:55 +0900

    Add a module for Grove Loudness Sensor

---
+++ grove/Loudness_Sensor/main.py
@@ -0,0 +1,46 @@
+from machine import ADC
+from utime import sleep
+import ujson
+import zcoap
+
+class Loudness:
+    def __init__(self, port):
+        if port == 0:
+            self.adc = ADC(0)
+        elif port == 1:
+            self.adc = ADC(4)
+        elif port == 2:
+            self.adc = ADC(6)
+        else:
+            raise Exception("Port# out of range")
+    def read(self):
+        valueSum = 0
+        count = 0
+        while count < 100:
+            value = self.adc.read()
+            if value < 4096 and value > 256:
+                valueSum += value
+                count += 1
+        
+        return valueSum / count
+         
+def main():
+    path = 'thing/' + zcoap.eui64()
+    reported = {'state':{'reported':{}}}
+
+    addr = zcoap.gw_addr()
+    port = 5683
+    cli = zcoap.client((addr, port))
+
+    loudness = Loudness(0)
+
+    while True:
+        reported['state']['reported']['loudness'] = loudness.read()
+
+        json = ujson.dumps(reported)
+        cli.request_post(path, json)
+        print(json)
+        sleep(1)
+
+if __name__ == "__main__":
+    main()
