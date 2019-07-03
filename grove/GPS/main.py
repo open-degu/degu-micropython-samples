f8fc848096381beddd824f286a6be75dfb7390ec
Naomi TODORI <naomi.todori@atmark-techno.com>
2019-07-03 14:44:58 +0900

    Integrate GPS_Parser class into GPS

--- grove/GPS/main.py
+++ grove/GPS/main.py
@@ -30,26 +30,16 @@ class GPS:
         else:
             return False
 
-    def read(self, sentences):
-        readdata = str(self.uart.readline(), 'ascii')
+    def rcv_verify(self, data, sentences):
         for sentence in sentences:
-            if sentence in readdata:
-                if self.checksum_verify(readdata):
-                    return readdata
-        return None
+            if sentence not in data:
+                return False
+        return True
 
-
-class GPS_Parser:
-    def __init__(self):
-        self.datetime = ''
-        self.lon = ''
-        self.lat = ''
-        self.pdop = ''
-
-    def parse(self, nmea):
+    def parse(self, nmea, sentence):
         parse_data = nmea.split(',')
-        self.sentence_list[parse_data[0]](self, parse_data)
-        return
+        func = getattr(self, sentence)
+        return func(parse_data)
 
     def change60to10(self, value):
         decimal, integer = math.modf(float(value) / 100.0)
@@ -61,20 +51,32 @@ class GPS_Parser:
                            parse_data[1][2:4] + ':' + parse_data[1][4:6]
             date = parse_data[9][4:6] + '/' + parse_data[9][2:4] + \
                                         '/' + parse_data[9][0:2]
-            self.datetime = date + ' ' + time
-            self.lat = self.change60to10(parse_data[3]) + parse_data[4]
-            self.lon = self.change60to10(parse_data[5]) + parse_data[6]
+            datetime = date + ' ' + time
+            lat = self.change60to10(parse_data[3]) + parse_data[4]
+            lon = self.change60to10(parse_data[5]) + parse_data[6]
         else:
-            self.datetime = ''
-            self.lat = ''
-            self.lon = ''
-        return
+            datetime = ''
+            lat = ''
+            lon = ''
+        return {'lat': lat, 'lon': lon, 'datetime': datetime}
 
     def gpgsa(self, parse_data):
-        self.pdop = parse_data[15]
-        return
+        return {'pdop': parse_data[15]}
 
-    sentence_list = {'$GPRMC': gprmc, '$GPGSA': gpgsa}
+    def read(self, sentences):
+        parsedata = {}
+        # Clear all buffers
+        buf = self.uart.read(self.uart.any())
+        while True:
+            readdata = str(self.uart.readline(), 'ascii')
+            for sentence in sentences:
+                if sentence in readdata.lower():
+                    if self.checksum_verify(readdata):
+                        parsedata[sentence] = {}
+                        parsedata[sentence] = self.parse(readdata, sentence)
+                        break
+            if self.rcv_verify(parsedata, sentences):
+                return parsedata
 
 
 def main():
@@ -86,33 +88,13 @@ def main():
     cli = zcoap.client((addr, port))
 
     gps = GPS(0, GPS_BAUDRATE)
-    parser = GPS_Parser()
-    rcv_flag = {'GPRMC': 0, 'GPGSA': 0}
 
     while True:
-        all_rcv = 0
-        nmea = gps.read(("GPRMC", "GPGSA"))
-        if nmea is None:
-            pass
-        else:
-            parser.parse(nmea)
-            for sentence in rcv_flag:
-                if sentence in nmea:
-                    rcv_flag[sentence] = 1
-                all_rcv = all_rcv + rcv_flag[sentence]
-
-            if all_rcv == len(rcv_flag):
-                reported['state']['reported'] = {'datetime': parser.datetime,
-                                                 'lat': parser.lat,
-                                                 'lon': parser.lon,
-                                                 'pdop': parser.pdop}
-                json = ujson.dumps(reported)
-                print(json)
-                cli.request_post(path, json)
-                for sentence in rcv_flag:
-                    rcv_flag[sentence] = 0
-
-        time.sleep_us(500)
+        reported['state']['reported'] = gps.read(("gprmc", "gpgsa"))
+        json = ujson.dumps(reported)
+        print(json)
+        cli.request_post(path, json)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
