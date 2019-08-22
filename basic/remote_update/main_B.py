3a296b1b258f1b8a2c107fc843999defe1a02077
Takumi Ando <takumi.ando@atmark-techno.com>
2019-08-22 10:48:06 +0900

    basic: Add samples for remote updating

---
+++ basic/remote_update/main_B.py
@@ -0,0 +1,12 @@
+import machine
+import time
+import degu
+
+if __name__ == '__main__':
+    while True:
+        print("Hello! I'm Bob.")
+        if (degu.check_update()):
+            print("New script is comming! Restarting...")
+            machine.reset();
+
+        time.sleep(1)
